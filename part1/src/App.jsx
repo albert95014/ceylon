1b63186d6c9706c6a4efc541e8caeec2ce26c81f
albert95014 <albertyang.me@gmail.com>
2023-08-25 23:10:56 -0700

    up to 1.8

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,30 +1,69 @@
 import { useState } from 'react'
 
-const Display = ({counter}) => <div>{counter}</div>
+const Button = ({handleClick, text}) => <button onClick={handleClick}>{text}</button>
 
-const Button = ({handleClick, text}) => (
-  <button onClick={handleClick}>
-    {text}
-  </button>
-)
+const StatisticLine = ({text, value}) => {
+  return (
+    <div>
+      {text} {value}
+    </div>
+  )
+}
+
+const Statistics = ({goodValue, neutralValue, badValue}) => {
+  if (goodValue + neutralValue + badValue === 0) {
+    return (
+      <div>
+        no feedback
+      </div>
+    )
+  }
+  return (
+    <div>
+      <p>good {goodValue}</p>
+      <p>neutral {neutralValue}</p>
+      <p>bad {badValue}</p>
+      <p>total {goodValue + neutralValue + badValue}</p>
+      <p>average {(goodValue*1 + (badValue*-1)) / (goodValue + neutralValue + badValue)}</p>
+      <p>positive {(goodValue/(goodValue + neutralValue + badValue))+"%"}</p>
+    </div>
+  )
+}
 
 const App = () => {
-  const [ counter, setCounter ] = useState(0)
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(good + neutral + bad)
 
-  const handleClick = () => {
-    console.log('clicked')
+  const handleGoodFeedback = () => {
+    const newGood = good+1
+    setGood(good+1)
+    setTotal(newGood+neutral+bad)
   }
 
-  const increaseByOne = () => setCounter(counter + 1)
-  const decreaseByOne = () => setCounter(counter - 1)
-  const setToZero = () => setCounter(0)
+  const handleNeutralFeedback = () => {
+    const newNeutral = neutral+1
+    setNeutral(neutral+1)
+    setTotal(good+newNeutral+bad)
+  }
+
+  const handleBadFeedback = () => {
+    const newBad = bad+1
+    setBad(bad+1)
+    setTotal(good+neutral+newBad)
+  }
 
   return (
     <div>
-      <Display counter={counter} />
-      <Button handleClick={increaseByOne} text="plus" />
-      <Button handleClick={decreaseByOne} text="minus" />
-      <Button handleClick={setToZero} text="zero" />
+      <h1>give feedback</h1>
+      <Button handleClick={handleGoodFeedback} text="good" />
+      <Button handleClick={handleNeutralFeedback} text="neutral" />
+      <Button handleClick={handleBadFeedback} text="bad" />
+
+      <h1>statistics</h1>
+      <Statistics goodValue={good} neutralValue={neutral} badValue={bad} />
     </div>
   )
 }
